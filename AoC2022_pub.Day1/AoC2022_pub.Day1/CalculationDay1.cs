da26a167df7929433263ad7a36fab4067fee7f80
mecesina <ksenia.gladkikh@yahoo.com>
2023-11-24 17:02:56 +0100

    Add conversion method, refactor the code, WIP

--- AoC2022_pub.Day1/AoC2022_pub.Day1/CalculationDay1.cs
+++ AoC2022_pub.Day1/AoC2022_pub.Day1/CalculationDay1.cs
@@ -1,65 +1,92 @@
-﻿namespace AoC2022_pub.Day1
+﻿using System.Runtime.CompilerServices;
+
+namespace AoC2022_pub.Day1
 {
     internal class CalculationDay1
     {
-        string[] lines = File.ReadAllLines("..\\testAoC2022.txt"); //NOTE: The file needs to be placed in Debug folder of the application
+        readonly string[] lines = File.ReadAllLines("..\\testAoC2022.txt"); //NOTE: The file needs to be placed in Debug folder of the application
+
 
         List<int> caloriesPerElf = new List<int>();
         List<int> totalElfsCalories = new List<int>();
+        
 
-        int totalCalPerElf = 0;
-        int maxCalories = 0;
-
-        public int CalculateMaxCal() //PROBLEM: It sums only the first one and doesn't continue. 
+        public int CalculateMaxCal() 
         {
-            foreach (string line in lines)
-            {
-
-                if (String.IsNullOrEmpty(line)) //Revert?
-                {
-                    totalElfsCalories.Add(CountCaloriesPerElf());
-                }
-
-                continue;
-
-            }
-            maxCalories = totalElfsCalories.Max();
+            
+            int maxCalories = CountCaloriesPerElf().Max();
             Console.WriteLine($"{maxCalories}");
             return maxCalories;
         }
-        private int CountCaloriesPerElf()
+        private List<int> CountCaloriesPerElf()
         {
             try
             {
-                foreach (string line in lines)
-                {
-                    if (String.IsNullOrEmpty(line))
-                        break;
+               List<int> parsedCal = ConvertCalories(lines);
 
-                    int parsedCal;
-                    bool success = int.TryParse(line, out parsedCal);
-                    if (success)
-                    {
-                        caloriesPerElf.Add(parsedCal);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Conversion of '{line}' to {parsedCal} failed");
-                    }
-                    //int parsedCal = int.Parse(line);
-
-                }
-                foreach (int cal in caloriesPerElf)
+                for(int i = 0; i<parsedCal.Count; i++)
                 {
-                    totalCalPerElf = caloriesPerElf.Sum();
+                    while(parsedCal[i]!=0 && i<parsedCal.Count-1) //doesn't take the last value in account!
+                    {
+                        caloriesPerElf.Add(parsedCal[i]);
+                        i++;
+                    }
+
+                    totalElfsCalories.Add(caloriesPerElf.Sum());
+                    caloriesPerElf.Clear();
                 }
-                return totalCalPerElf;
+
+
+                return totalElfsCalories;
+                //foreach (string line in lines)
+                //{
+                //    //caloriesPerElf.Add(int.Parse(line.Trim()));
+                //    if (String.IsNullOrEmpty(line))
+                //        break;
+
+                //    int parsedCal;
+                //    bool success = int.TryParse(line, out parsedCal);
+                //    if (success)
+                //    {
+                //        caloriesPerElf.Add(parsedCal);
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine($"Conversion of '{line}' to {parsedCal} failed");
+                //    }
+                //    //int parsedCal = int.Parse(line);
+
+                //}
+                //foreach (int cal in caloriesPerElf)
+                //{
+                //    int totalCalPerElf = caloriesPerElf.Sum();
+                //}
+                //return totalCalPerElf;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
-                return 0;
+                return null;
             }
+        }
+        private List<int> ConvertCalories(string[] lines)
+        {
+            int parsedCal;
+            List<int> parsedCalories = new List<int>();
+            foreach (string line in lines)
+            {
+                if(!String.IsNullOrEmpty(line))
+                {
+                    parsedCal = int.Parse(line);
+
+                    parsedCalories.Add(parsedCal);
+                }
+                if(String.IsNullOrEmpty(line))
+                {
+                    parsedCalories.Add(0);
+                }
+            }
+            return parsedCalories;
         }
     }
 }
